e455dec636eeaf24ce8880262e20832eda9449e6
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 05:37:57 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan8-Pecahan/src/pbo/ulang/pkg10114504/latihan8/pecahan/PBOULANG10114504Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program Pecahan
+ */
+public class PBOULANG10114504Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+                
+    }
+    
+}
